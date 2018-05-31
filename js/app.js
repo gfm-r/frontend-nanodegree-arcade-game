2d08ce31b8c000e2ef5e00b2a622d93228a54476
gfm <->
2018-06-01 02:45:43 +0300

    add: score indicator

--- js/app.js
+++ js/app.js
@@ -34,9 +34,11 @@ let en3 = new Enemy(1, 230, 160);
 
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [en1, en2, en3];
-
 ///////////////////Player/////////////////////////////////////////////////////
 // Place the player object in a variable called player
+//متغير نخزن فيه نقاط الاعب my_Score
+let my_Score = document.querySelector('.inerTXT');
+
 class Player {
   constructor(x, y) {
     // Variables applied to each of our instances go here,
@@ -54,6 +56,7 @@ class Player {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //عندما يلامس العدو اللاعب يرجع الاعب الى موضعه الاصلي
     if (player.x >= (en1.x - 70) && player.x <= (en1.x + 70) && player.y >= (en1.y - 70) && player.y <= (en1.y + 50)) {
       this.x = 200;
       this.y = 400;
@@ -85,11 +88,13 @@ class Player {
       this.x += -100;
     }
   }
+  //زيادة عدد النقاط
   get_Score() {
     this.x = 200;
     this.y = 400;
     this.score++;
-    ////////////////////////////////////////////
+    my_Score.innerText++;
+    ////////////swal-alert//////////////////////////
     if (this.score == 10) {
       swal({
         title: 'Congratulation! You Won!',
@@ -97,6 +102,7 @@ class Player {
         button: 'Play again!',
       }).then(function() {
         window.location.reload();
+        handleInput = 'undefined';
       });
 
     } //for if
@@ -106,8 +112,6 @@ class Player {
 }
 let player = new Player(200, 400);
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
